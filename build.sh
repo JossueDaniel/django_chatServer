72db7150abb61550e310bd183edff61259bb900e
danny <jossuedaniel@outlook.com>
2024-06-12 16:11:16 -0500

    Editando el build

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --settings=chat_project.settings.prod
 
 # Apply any outstanding database migrations
-python manage.py migrate
+python manage.py migrate --settings=chat_project.settings.prod
