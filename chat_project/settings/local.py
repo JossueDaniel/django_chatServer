157610502609bec8fb3550454bce0c1658351871
danny <jossuedaniel@outlook.com>
2024-12-17 22:03:26 -0500

    Agregando los archivos Dockerfile y compose para crear contenedores

--- chat_project/settings/local.py
+++ chat_project/settings/local.py
@@ -5,12 +5,29 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # DATABASES = {
-#     'default': env.dj_db_url('DATABASE_URL', default="postgres://postgres@db/postgres")
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('POSTGRES_DB'),
+        'USER': env.str('POSTGRES_USER'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': REDIS_URL,
+        # 'OPTIONS': {
+        #     'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        # }
     }
 }
