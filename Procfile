88110c39b289a37758d292a4866846da840af218
danny <jossuedaniel@outlook.com>
2024-12-18 23:22:11 -0500

    Configurando whitenoise

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn chat_project.wsgi --env DJANGO_SETTINGS_MODULE=chat_project.settings.prod
+web: python manage.py collectstatic && gunicorn chat_project.wsgi --env DJANGO_SETTINGS_MODULE=chat_project.settings.prod
--- chat_project/settings/base.py
+++ chat_project/settings/base.py
@@ -52,13 +52,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'chat_project.urls'
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
